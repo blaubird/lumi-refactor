d2aa50aaa1e5de353d7d606464ab392b52396d08
blaubird <evereinenterprise@gmail.com>
2025-05-24 13:55:19 +0200

    Update main.py

--- api/main.py
+++ api/main.py
@@ -75,5 +75,5 @@ async def ping():
 if __name__ == "__main__":
     import hypercorn
     port = int(os.getenv("PORT", "8000"))
-    logger.info(f"Starting uvicorn server on port {port}")
+    logger.info(f"Starting Hypercorn server on port {port}")
     hypercorn.run("main:app", host="0.0.0.0", port=port, reload=True)
